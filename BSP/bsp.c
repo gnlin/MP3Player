62233c0a46db9edabbe202b4205c497e51c2ac59
Guannan Lin <gnlinsh@gmail.com>
2015-03-06 22:02:47 +1100

    Update bsp.c

--- BSP/bsp.c
+++ BSP/bsp.c
@@ -8,8 +8,8 @@
 *                                   STM3210B-EVAL Evaluation Board
 *
 * Filename      : bsp.c
-* Version       : V0.01
-* Programmer(s) : gnlin
+* Version       : V1.00  
+* Programmer(s) : Brian Nagel 
 *********************************************************************************************************
 */
 
